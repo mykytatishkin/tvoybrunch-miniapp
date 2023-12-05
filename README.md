28759ecada6e158f6ef68266203c089adc470917
Mykyta Tishkin <87891550+mykytatishkin@users.noreply.github.com>
2023-12-05 13:14:35 +0200

    color fix

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+Code literacy:
+Color for btn : --tg-theme-button-text-color
+Color for all : --tg-theme-text-color
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Header from "./components/Header/Header";
 import {Route, Routes} from 'react-router-dom'
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
-import Contacts from "./components/Contacts/Contacts"
+import Contacts from "./components/Contacts/Contacts";
+import Profile from "./components/Profile/Profile"
 
 function App() {
     const {onToggleButton, tg} = useTelegram();
@@ -29,6 +30,7 @@ function App() {
                 <Route index element={<ProductList />}/>
                 <Route path={'form'} element={<Form />}/>
                 <Route path={'contacts'} element={<Contacts />}/>
+                <Route path={'profile'} element={<Profile />}/>
             </Routes>
         </div>
     );
