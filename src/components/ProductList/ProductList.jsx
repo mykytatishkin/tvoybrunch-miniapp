8bd5a9aee3e05a70661a42e01e8254c9a285376e
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2023-12-10 00:57:16 +0200

    route

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -14,7 +14,7 @@ const products = [
     // ** TAG
     
     {id: '1',  title: 'Бокс с Тарталетками',                    image: '', size:'M',    price: 91,  type:'size_box',  description: '20 штук - 740 грамм'},
-    {id: '2',  title: 'Бокс с Тарталетками',                    image: 'imgs\\Box_brusketty_delicious.jpg', size:'L',    price: 101, type:'size_box',    description: '25 штук - 1190 грамм'},
+    {id: '2',  title: 'Бокс с Тарталетками',                    image: '..\\imgs\\Box_brusketty_delicious.jpg', size:'L',    price: 101, type:'size_box',    description: '25 штук - 1190 грамм'},
     {id: '3',  title: 'Бокс с Тарталетками',                    image: '', size:'XL',   price: 131, type:'size_box',    description: '30 штук - 1510 грамм'},
 
     {id: '4',  title: 'Бокс Брускетта delicious',               image: '', size:'M',    price: 91,  type:'size_box',    description: '12 штук - 750 грамм'},
