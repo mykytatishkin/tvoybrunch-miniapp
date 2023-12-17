d2b62c65ddd790e44daf1d17a6279a99f390bba7
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2023-12-18 01:38:40 +0200

    pochti fix kurwa

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,19 +1,46 @@
-import React, {useCallback, useEffect, useState} from 'react';
-import './Details.css'
-import {useTelegram} from "../../hooks/useTelegram";
+// Details.jsx
+import React, { useState, useEffect } from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
+import { useParams } from 'react-router-dom';
 
-const Form = () => {
-    const [country, setCountry] = useState('');
-    const [street, setStreet] = useState('');
-    const [subject, setSubject] = useState('physical');
-    const {tg, user} = useTelegram();
+const Details = () => {
+    const { tg, user } = useTelegram();
+    const { productId } = useParams();
+    const [productDetails, setProductDetails] = useState(null);
+
+    useEffect(() => {
+        // Здесь вы можете загрузить информацию о продукте с использованием productId
+        // Например, запрос к API или использование хранилища
+
+        // Пример заглушки
+        const dummyProductDetails = {
+            id: '1',
+            title: 'Бокс с Тарталетками',
+            image: 'https://github.com/mykytatishkin/tvoybrunch-miniapp/blob/main/src/components/ProductList/imgs/box%20s%20bruskettami.jpg?raw=true',
+            size: 'M',
+            price: 91,
+            type: 'size_box',
+            description: '20 штук - 740 грамм',
+        };
+
+        // Если productId существует и равен '1', устанавливаем детали продукта
+        if (productId && productId === '1') {
+            setProductDetails(dummyProductDetails);
+        }
+    }, [productId]);
 
     return (
-        <div className='details'>
+        <div className="details">
             <h2>Детальная информация про товар</h2>
-            
+            {productDetails && (
+                <div>
+                    <h3>{productDetails.title}</h3>
+                    <p>{productDetails.description}</p>
+                    {/* Другие поля продукта */}
+                </div>
+            )}
         </div>
     );
 };
 
-export default Form;
+export default Details;
