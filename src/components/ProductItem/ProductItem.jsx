941dd63c3b587c20f06f6fea3ca1bb59b6400793
Mykyta Tishkin <87891550+mykytatishkin@users.noreply.github.com>
2023-12-27 13:29:20 +0100

    kurwa test 1

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -3,11 +3,11 @@ import Button from "../Button/Button";
 import './ProductItem.css';
 import { Link } from 'react-router-dom';
 
-const ProductItem = ({product, onRemove, className, onAdd}) => {
-    
+const ProductItem = ({ product, onRemove, className, onAdd }) => {
+
     const onAddHandler = () => {
         onAdd(product);
-        
+
         product.amount += 1;
     }
 
@@ -17,44 +17,39 @@ const ProductItem = ({product, onRemove, className, onAdd}) => {
         product.amount -= 1;
     }
 
-    if(product.type == 'tag') {
-        return ( 
-            
-            <div className='topic'>
-                <hr />
-                <div  id={product.id}><img className='ico' src={product.image} alt="" /></div>
-                <div className={'title'}><b><h1>{product.title}</h1></b></div>
-                <div className={'description'}><p>{product.description}</p></div>
-            </div>
-            
-        );
-    } 
-    // TODO : change image to this, when ull get pictures
-    // TODO : <div className={'img'}><img className={'img'} src={product.image} alt="picture"/></div>
-    else {
-        return (
-            <div id={product.id} className={'product'}>
-    <div className={'img'}>
-        <img className={'img'} src={product.image} alt="picture" />
-    </div>
-    <Link className="details" to={`/details/${product.id}`}>
-        <div className={'title'}>
-            <h4>{product.title}</h4>
+    return (
+        <div id={product.id} className={product.type === 'tag' ? 'topic' : 'product'}>
+            {product.type === 'tag' ? (
+                <>
+                    <hr />
+                    <div id={product.id}><img className='ico' src={product.image} alt="" /></div>
+                    <div className={'title'}><b><h1>{product.title}</h1></b></div>
+                    <div className={'description'}><p>{product.description}</p></div>
+                    <Button className={'add-btn'} onClick={onAddHandler}>Купить</Button>
+                </>
+            ) : (
+                <>
+                    <div className={'img'}>
+                        <img className={'img'} src={product.image} alt="picture" />
+                    </div>
+                    <Link className="details" to={`/details/${product.id}`}>
+                        <div className={'title'}>
+                            <h4>{product.title}</h4>
+                        </div>
+                    </Link>
+                    <div className={'description'}>{product.description}</div>
+                    <div className={'price'}>
+                        Стоимость: <b>{product.price} BYN</b>
+                    </div>
+                    <div className={'title'}>
+                        <Button className={'add-btn'} onClick={onAddHandler}>+</Button>
+                        <span className={'add-btn'}>{product.amount}</span>
+                        <Button className={'add-btn'} onClick={onRemoveHandler}>-</Button>
+                    </div>
+                </>
+            )}
         </div>
-    </Link>
-    <div className={'description'}>{product.description}</div>
-    <div className={'price'}>
-        Стоимость: <b>{product.price} BYN</b>
-    </div>
-    <div className={'title'}>
-        <Button className={'add-btn'} onClick={onAddHandler}>+</Button>
-        <span className={'add-btn'}>{product.amount}</span>
-        <Button className={'add-btn'} onClick={onRemoveHandler}>-</Button>
-    </div>
-</div>
-        );
-    }
+    );
 };
-
 
 export default ProductItem;
